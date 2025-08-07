9e5ba4210e3861629634f186a3242b2e14cff182
Juan Manuel Chávez Peralta <juan@grupolan.com>
2025-08-07 01:16:20 -0700

    Videos section added and modifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeroComponent } from './components/hero/hero.component';
 import { AboutComponent } from './components/about/about.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ServicesComponent } from "./components/services/services.component";
+import { VideosComponent } from './components/videos/videos.component'; 
  // ✅ Asegúrate que la ruta sea correcta
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroComponent, AboutComponent, NavbarComponent, ServicesComponent],
+  imports: [RouterOutlet, HeroComponent, AboutComponent, NavbarComponent, ServicesComponent,VideosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/videos/videos.component.ts
+++ src/app/components/videos/videos.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-videos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './videos.component.html',
-  styleUrl: './videos.component.scss'
+  styleUrls: ['./videos.component.scss']
 })
 export class VideosComponent {
-
+  videos = [
+    {
+      title: 'Testimonio 1',
+      type: 'vimeo',
+      url: 'https://player.vimeo.com/video/967292388'
+    },
+    {
+      title: 'Testimonio 2',
+      type: 'vimeo',
+      url: 'https://player.vimeo.com/video/967292218'
+    },
+    {
+      title: 'Consultoría',
+      type: 'youtube',
+      url: 'https://www.youtube.com/embed/IKf21MLQ3hI'
+    }
+  ];
 }
